cf843638292e753c1d0db63591fdf6c825691ac5
wckedbot <shreyansh.anand2019@vitstudent.ac.in>
2020-08-24 11:46:06 +0530

    Skeleton

---
+++ Chapter 1/index-Shreyansh.js
@@ -0,0 +1,65 @@
+function init() {
+    var stats = initStats();
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    camera.position.x = -30;
+    camera.position.y = 40;
+    camera.position.z = 30;
+    camera.lookAt(scene.position);
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0xEEEEEE, 1.0);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    var axes = new THREE.AxisHelper(20);
+    scene.add(axes);
+
+    var planeMaterial = new THREE.MeshBasicMaterial({
+        color: 0xcccccc
+    });
+    var planeGeometry = new THREE.PlaneGeometry(60, 20, 1, 1);
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.x = 15;
+    plane.position.y = 0;
+    plane.position.z = 0;
+    scene.add(plane);
+
+    var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+    var cubeMaterial = new THREE.MeshBasicMaterial({
+        color: 'cyan',
+        wireframe: true
+    });
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.position.x = -4;
+    cube.position.y = 3;
+    cube.position.z = 0;
+    scene.add(cube);
+
+    var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+    var sphereMaterial = new THREE.MeshBasicMaterial({
+        color: 0x7777ff,
+        wireframe: true
+    })
+    var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    sphere.position.x = 20;
+    sphere.position.y = 4;
+    sphere.position.z = 2;
+
+    scene.add(sphere);
+    document.getElementById("WebGL-output").appendChild(renderer.domElement);
+    renderer.render(scene, camera);
+
+    function initStats() {
+        var stats = new Stats();
+        stats.setMode(0);
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.left = '0px';
+        stats.domElement.style.top = '0px';
+        document.getElementById("Stats-output").appendChild(stats.domElement);
+        return stats;
+    }
+    initStats();
+}
+init();
